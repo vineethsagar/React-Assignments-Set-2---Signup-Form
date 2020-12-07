ebecf09a6427c6657aa319940b75c96ccb928e71
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-12-08 00:28:23 +0530

    no modifications

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-/* const App = () => {
+const App = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [gender, setGender] = useState("male");
@@ -76,7 +76,7 @@ import "../styles/App.css";
   };
   return (
     <div id="main">
-     
+      {/* <form onSubmit={handleSubmit}> */}
       <label>
         Name:
         <input
@@ -125,168 +125,9 @@ import "../styles/App.css";
       <button data-testid="button" onClick={handleSubmit}>
         button
       </button>
+      {/* </form> */}
       <h3>{errorMessage}</h3>
       <h3>{welcomeMessage}</h3>
-    </div>
-  );
-};
- */
-
-const App = () => {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [welcomeMessage, setWelcomeMessage] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("male");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleName = (evt) => {
-    setName(evt.target.value);
-  };
-
-  const handleEmail = (evt) => {
-    setEmail(evt.target.value);
-  };
-
-  const handleGender = (evt) => {
-    setGender(evt.target.value);
-  };
-
-  const handlePhoneNumber = (evt) => {
-    setPhoneNumber(evt.target.value);
-  };
-
-  const handlePassword = (evt) => {
-    setPassword(evt.target.value);
-  };
-
-  const isNameValid = (nameValue) => {
-    const regex = /^[a-zA-Z0-9 ]*$/;
-    return regex.test(nameValue);
-  };
-
-  const isEmailValid = (emailValue) => {
-    return emailValue.includes("@");
-  };
-
-  const isGenderValid = (genderValue) => {
-    return (
-      genderValue === "male" ||
-      genderValue === "female" ||
-      genderValue === "others"
-    );
-  };
-
-  const isPhoneNumberValid = (phoneNumberVal) => {
-    const regex = /^\d+$/;
-    return regex.test(phoneNumberVal);
-  };
-
-  const isPasswordValid = (passwordVal) => {
-    return passwordVal.length >= 6;
-  };
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    setWelcomeMessage("");
-    if (!name || !email || !gender || !phoneNumber || !password) {
-      setErrorMessage("All fields are mandatory");
-      return;
-    } else if (!isNameValid(name)) {
-      setErrorMessage("Name is not alphanumeric");
-      return;
-    } else if (!isEmailValid(email)) {
-      setErrorMessage("Email must contain @");
-      return;
-    } else if (!isGenderValid(gender)) {
-      setErrorMessage("Please identify as male, female or others");
-      return;
-    } else if (!isPhoneNumberValid(phoneNumber)) {
-      setErrorMessage("Phone Number must contain only numbers");
-      return;
-    } else if (!isPasswordValid(password)) {
-      setErrorMessage("Password must contain atleast 6 letters");
-      return;
-    } else {
-      setErrorMessage("");
-    }
-
-    const userName = email.split("@")[0];
-    setWelcomeMessage(`Hello ${userName}`);
-  };
-  return (
-    <div id="main">
-      {/* <form onSubmit = {handleSubmit}> */}
-      <div>
-        <label>Name</label>
-        <input
-          data-testid="name"
-          // name = 'name'
-          // type='string'
-          value={name}
-          onChange={handleName}
-        />
-      </div>
-
-      <div>
-        <label>Email address</label>
-        <input
-          data-testid="email"
-          // name = 'email'
-          // type='string'
-          value={email}
-          onChange={handleEmail}
-        />
-      </div>
-
-      <div>
-        <label>Gender</label>
-        <input
-          data-testid="gender"
-          // name = 'gender'
-          // type='text'
-          // defaultValue = 'male'
-          value={gender}
-          onChange={handleGender}
-        />
-      </div>
-
-      <div>
-        <label>Phone Number</label>
-        <input
-          data-testid="phoneNumber"
-          // name = 'phoneNumber'
-          value={phoneNumber}
-          onChange={handlePhoneNumber}
-        />
-      </div>
-
-      <div>
-        <label>Password</label>
-        <input
-          data-testid="password"
-          // name = 'password'
-          value={password}
-          onChange={handlePassword}
-          type="password"
-        />
-      </div>
-
-      <div>{errorMessage}</div>
-      <div>
-        <button
-          data-testid="submit"
-          // type='submit'
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
-      </div>
-
-      {/* </form> */}
-
-      <div>{welcomeMessage}</div>
     </div>
   );
 };
