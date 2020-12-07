e98e021803005f2af7a21f48ee27a6f1393df614
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-12-07 23:28:04 +0530

    made a form attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Form from "./Form";
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <Form />
+    </div>
+  );
+};
 
 export default App;
